46b4fac895f4dc8d1b5c747ab57d06cf8c97260f
Stronmarker <Pika20k@gmail.com>
2024-06-14 17:00:56 +0200

    Modification responsive Login/Register, début de Price

--- assets/app.js
+++ assets/app.js
@@ -5,6 +5,7 @@
 import './styles/bootstrap.css';
 import './styles/app.css'
 import './bootstrap.js'
+import './sweetalert.js'
 
 import { Application } from "@hotwired/stimulus";
 import ThreeController from "./controllers/three-controller";
