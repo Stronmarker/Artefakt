831c5af98e494977ac60c6a3df896e2943a410fc
Terrydall <chrale2822@hotmail.fr>
2024-06-27 16:14:40 +0200

    modification usersubscription controller et twig

--- src/Controller/UserSubscriptionController.php
+++ src/Controller/UserSubscriptionController.php
@@ -4,16 +4,47 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class UserSubscriptionController extends AbstractController
 {
     #[Route('/user/subscription', name: 'app_user_subscription')]
     public function index(): Response
     {
-        
+        $user = $this->getUser(); 
+
+        if ($user && $user->isSubscribed()) {
+            $utilisateurEstAbonne = true;
+            $abonnement = [
+                'type' => 'Premium',
+                'price' => '€19.99/mois',
+                'features' => [
+                    'Création de projets illimités',
+                    'Outils illimités',
+                    'Assistance par e-mail et téléphone',
+                    'Accès au centre d\'aide'
+                ],
+                'status' => 'Actif'
+            ];
+        } else {
+            $utilisateurEstAbonne = false;
+            $abonnement = [
+                'type' => 'Abonnement Gratuit',
+                'price' => '€0/mois',
+                'features' => [
+                    'Création de 3 projets',
+                    'Outils limités',
+                    'Assistance par e-mail',
+                    'Accès au centre d\'aide',
+                    'Pour plus de fonctionnalités passez à l\'offre premium'
+                ],
+                'status' => 'Offre Premium non active'
+            ];
+        }
+
         return $this->render('user_subscription/index.html.twig', [
-            'controller_name' => 'UserSubscriptionController',
+            'abonnement' => $abonnement,
+            'utilisateur_est_abonne' => $utilisateurEstAbonne
         ]);
     }
 }
