e943fbe7a791ab09002fdc18297fb405997ae70c
Terrydall <chrale2822@hotmail.fr>
2024-07-10 12:33:30 +0200

    ajout de stripe

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -3,10 +3,13 @@
 namespace App\Controller;
 
 use Doctrine\ORM\EntityManagerInterface;
+use Stripe\Stripe;
+use Stripe\Customer;
+use Stripe\Subscription;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\User\UserInterface;
 
 class SubscriptionController extends AbstractController
@@ -18,20 +21,47 @@ class SubscriptionController extends AbstractController
 
         return $this->render('subscription/index.html.twig', [
             'isSubscribed' => $isSubscribed,
+            'public_key' => 'pk_test_51N1m7HB8NXWXURY9QWbBOLT9JC5tPWtrBDyfJEO3u6A6owTH8Yu3daMvDoMvmo6fGfQFzkTcAMCb9cfum1cTp51W000XeNlvkv',
         ]);
     }
 
-    #[Route('/subscribe', name: 'app_subscribe')]
-    public function subscribe(EntityManagerInterface $em, UserInterface $user): Response
+    #[Route('/subscribe', name: 'app_subscribe', methods: ['POST'])]
+    public function subscribe(Request $request, EntityManagerInterface $em, UserInterface $user): Response
     {
         if ($user->isSubscribed()) {
             return $this->json(['success' => false, 'message' => 'Vous êtes déjà abonné']);
         }
 
-        $user->setSubscribed(true);
-        $em->persist($user);
-        $em->flush();
+        $data = json_decode($request->getContent(), true);
+        $email = $data['email'];
+        $paymentMethod = $data['payment_method'];
 
-        return $this->json(['success' => true]);
+        Stripe::setApiKey('sk_test_51N1m7HB8NXWXURY9cnq0pT8MVnBwb4s4mUaW3D1z3Pel7jwYQjfsezlh3SXVdfo3Ic4fVej3bdC2P8euKhUpHq8R00FrPw8ACR');
+
+        try {
+            $customer = Customer::create([
+                'email' => $email,
+                'payment_method' => $paymentMethod,
+                'invoice_settings' => [
+                    'default_payment_method' => $paymentMethod,
+                ],
+            ]);
+
+            $subscription = Subscription::create([
+                'customer' => $customer->id,
+                'items' => [[
+                    'price' => 'price_1Pab22B8NXWXURY9vaFDcA0n',
+                ]],
+                'expand' => ['latest_invoice.payment_intent'],
+            ]);
+
+            $user->setSubscribed(true);
+            $em->persist($user);
+            $em->flush();
+
+            return $this->json(['success' => true, 'subscription' => $subscription]);
+        } catch (\Exception $e) {
+            return $this->json(['success' => false, 'message' => $e->getMessage()], 400);
+        }   
     }
 }
