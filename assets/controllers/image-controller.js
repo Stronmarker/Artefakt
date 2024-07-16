9f862bb19f3ce7cddfc77f2cdf1dd5bc49cc46b4
Stronmarker <Pika20k@gmail.com>
2024-07-16 15:59:01 +0200

    modal du rendu OK, home css modifié

--- assets/controllers/image-controller.js
+++ assets/controllers/image-controller.js
@@ -1,9 +1,10 @@
 import { Controller } from "@hotwired/stimulus";
 import * as THREE from "three";
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { Modal } from "bootstrap";
 
 export default class extends Controller {
-    static targets = ["canvasContainer", "modalCanvasContainer", "resetButton"];
+    static targets = ["canvasContainer", "resetButton", "pauseButton"];
 
     connect() {
         console.log("Le contrôleur Stimulus est connecté !");
@@ -35,23 +36,10 @@ export default class extends Controller {
         directionalLight.position.set(5, 10, 7.5);
         this.scene.add(directionalLight);
 
-        // Ajout de la carte (forme 3D) sur le podium
-        const frontTexture = new THREE.TextureLoader().load(this.element.dataset.imageFrontPng);
-        const towardTexture = new THREE.TextureLoader().load(this.element.dataset.imageTowardPng);
-        const whiteMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff }); // Matériau blanc pour les bords
-
-        const cardMaterials = [
-            new THREE.MeshBasicMaterial({ map: frontTexture }),  // Face avant
-            new THREE.MeshBasicMaterial({ map: towardTexture }), // Face arrière
-            whiteMaterial, // Bordure supérieure
-            whiteMaterial, // Bordure inférieure
-            whiteMaterial, // Bordure gauche
-            whiteMaterial  // Bordure droite
-        ];
-        const cardGeometry = new THREE.BoxGeometry(0.010, 2, 3.05); // Dimensions spécifiques de la carte
-        this.card = new THREE.Mesh(cardGeometry, cardMaterials);
-        this.card.position.y = 1.5; // Positionner la carte sur le podium
-        this.scene.add(this.card);
+        // Chargement des textures et ajout de la carte (forme 3D) sur le podium
+        this.loadTextures().then(() => {
+            this.addCardToScene();
+        });
 
         // Initialisation des contrôles pour bouger la carte
         this.controls = new OrbitControls(this.camera, this.renderer.domElement);
@@ -68,15 +56,33 @@ export default class extends Controller {
 
         // Ajuster la taille de la fenêtre
         window.addEventListener('resize', () => this.onWindowResize(this.canvasContainerTarget));
+    }
 
-        // Gérer les boutons
-        // this.pauseButtonTarget.addEventListener('click', this.toggleRotation.bind(this));
-        this.resetButtonTarget.addEventListener('click', this.resetCamera.bind(this));
+    async loadTextures() {
+        this.frontTexture = await new Promise(resolve => {
+            new THREE.TextureLoader().load(this.element.dataset.imageFrontPng, resolve);
+        });
+        this.towardTexture = await new Promise(resolve => {
+            new THREE.TextureLoader().load(this.element.dataset.imageTowardPng, resolve);
+        });
+    }
 
-        // Gérer l'ouverture de la modale
-        const modalElement = document.getElementById('canvasModal');
-        modalElement.addEventListener('show.bs.modal', () => this.showModalCanvas());
-        modalElement.addEventListener('hidden.bs.modal', () => this.hideModalCanvas());
+    addCardToScene() {
+        if (this.frontTexture && this.towardTexture) {
+            const whiteMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff }); // Matériau blanc pour les bords
+            const cardMaterials = [
+                new THREE.MeshBasicMaterial({ map: this.frontTexture }),  // Face avant
+                new THREE.MeshBasicMaterial({ map: this.towardTexture }), // Face arrière
+                whiteMaterial, // Bordure supérieure
+                whiteMaterial, // Bordure inférieure
+                whiteMaterial, // Bordure gauche
+                whiteMaterial  // Bordure droite
+            ];
+            const cardGeometry = new THREE.BoxGeometry(0.010, 2, 3.05); // Dimensions spécifiques de la carte
+            this.card = new THREE.Mesh(cardGeometry, cardMaterials);
+            this.card.position.y = 1.5; // Positionner la carte sur le podium
+            this.scene.add(this.card);
+        }
     }
 
     animate() {
@@ -115,19 +121,56 @@ export default class extends Controller {
         this.controls.update(); // Mettre à jour les contrôles pour refléter les changements de la caméra
     }
 
-    showModalCanvas() {
-        this.updateCanvasSize(this.modalCanvasContainerTarget);
-        this.renderer.setSize(this.canvasWidth, this.canvasHeight);
-        this.modalCanvasContainerTarget.appendChild(this.renderer.domElement);
-        this.camera.aspect = this.canvasWidth / this.canvasHeight;
-        this.camera.updateProjectionMatrix();
-    }
+    showModalCanvas(event) {
+        
 
-    hideModalCanvas() {
-        this.updateCanvasSize(this.canvasContainerTarget);
-        this.renderer.setSize(this.canvasWidth, this.canvasHeight);
-        this.canvasContainerTarget.appendChild(this.renderer.domElement);
-        this.camera.aspect = this.canvasWidth / this.canvasHeight;
-        this.camera.updateProjectionMatrix();
+        // Création de la modale
+        const modalElement = document.createElement('div');
+        modalElement.classList.add('modal', 'fade');
+        modalElement.id = 'canvasModal';
+        modalElement.setAttribute('tabindex', '-1');
+        modalElement.setAttribute('aria-labelledby', 'canvasModalLabel');
+        modalElement.setAttribute('aria-hidden', 'true');
+
+        modalElement.innerHTML = `
+            <div class="modal-dialog modal-fullscreen">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <div>
+                            <button class="btn btn-secondary me-2" data-action="click->image#toggleRotation">Pause Rotation</button>
+                            <button class="btn btn-secondary me-2" data-action="click->image#resetCamera">Reset Camera</button>
+                        </div>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div class="modal-body" style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+                        <div id="modalCanvasContainer" style="width: 100%; height: 100%;"></div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+        document.body.appendChild(modalElement);
+
+        // Afficher la modale
+        const modal = new Modal(modalElement);
+        modal.show();
+
+        modalElement.addEventListener('shown.bs.modal', () => {
+            const modalCanvasContainer = document.getElementById('modalCanvasContainer');
+            this.updateCanvasSize(modalCanvasContainer);
+            this.renderer.setSize(this.canvasWidth, this.canvasHeight);
+            modalCanvasContainer.appendChild(this.renderer.domElement);
+            this.camera.aspect = this.canvasWidth / this.canvasHeight;
+            this.camera.updateProjectionMatrix();
+        });
+
+        modalElement.addEventListener('hidden.bs.modal', () => {
+            this.updateCanvasSize(this.canvasContainerTarget);
+            this.renderer.setSize(this.canvasWidth, this.canvasHeight);
+            this.canvasContainerTarget.appendChild(this.renderer.domElement);
+            this.camera.aspect = this.canvasWidth / this.canvasHeight;
+            this.camera.updateProjectionMatrix();
+            modalElement.remove();
+        });
     }
 }
