241cc55c437dd6d038c388ac089b076625a433bb
Terrydall <chrale2822@hotmail.fr>
2024-07-18 14:57:37 +0200

    suppressions multiples

--- src/Controller/DashboardController.php
+++ src/Controller/DashboardController.php
@@ -5,9 +5,6 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Security\Core\User\UserInterface;
-use Stripe\Stripe;
-use Stripe\BillingPortal\Session;
 
 class DashboardController extends AbstractController
 {
@@ -19,21 +16,4 @@ class DashboardController extends AbstractController
         ]);
     }
 
-    #[Route('/billing-portal', name: 'app_billing_portal')]
-    public function billingPortal(UserInterface $user): Response
-    {
-        Stripe::setApiKey($_ENV['STRIPE_API_KEY']);
-
-        try {
-            $session = Session::create([
-                'customer' => $user->getStripeCustomerId(),
-                'return_url' => $this->generateUrl('app_dashboard', [], 0),
-            ]);
-
-            return $this->redirect($session->url);
-        } catch (\Exception $e) {
-            // En cas d'erreur, retourne une réponse JSON avec le message d'erreur
-            return $this->json(['success' => false, 'message' => $e->getMessage()], 400);
-        }
-    }
 }
