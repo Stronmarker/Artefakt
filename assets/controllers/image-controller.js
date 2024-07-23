f55ef98e8dda70db2200aa37a3e9024602d2778e
Stronmarker <Pika20k@gmail.com>
2024-07-23 13:46:03 +0200

    Modif de css

--- assets/controllers/image-controller.js
+++ assets/controllers/image-controller.js
@@ -43,12 +43,13 @@ export default class extends Controller {
         this.canvasContainerTarget.appendChild(this.renderer.domElement);
 
         // Ajout de lumières
-        const ambientLight = new THREE.AmbientLight(0x404040, 2);
+        const ambientLight = new THREE.AmbientLight(0x404040, 50);
         this.scene.add(ambientLight);
 
-        const directionalLight = new THREE.DirectionalLight(0xffffff, 1);
-        directionalLight.position.set(5, 10, 7.5);
-        this.scene.add(directionalLight);
+        // Désactiver ou réduire l'intensité de la lumière directionnelle
+        // const directionalLight = new THREE.DirectionalLight(0xffffff, 0.5);
+        // directionalLight.position.set(5, 10, 7.5);
+        // this.scene.add(directionalLight);
     }
 
     createTexturedBackground() {
@@ -58,6 +59,10 @@ export default class extends Controller {
             texture.wrapT = THREE.RepeatWrapping;
             texture.repeat.set(1, 1);
         });
+        const cubeTextureLoader = new THREE.CubeTextureLoader()
+
+        const envMap = cubeTextureLoader.load('/build/images/lonely_road_afternoon_puresky_2k.9a363b18')
+        this.scene.environment = envMap; 
 
         const geometry = new THREE.SphereGeometry(500, 60, 40);
         const material = new THREE.MeshBasicMaterial({
@@ -82,8 +87,8 @@ export default class extends Controller {
         if (this.frontTexture && this.towardTexture) {
             const whiteMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
             const cardMaterials = [
-                new THREE.MeshBasicMaterial({ map: this.frontTexture }),
-                new THREE.MeshBasicMaterial({ map: this.towardTexture }),
+                new THREE.MeshPhysicalMaterial({ map: this.frontTexture }),
+                new THREE.MeshPhysicalMaterial({ map: this.towardTexture }),
                 whiteMaterial,
                 whiteMaterial,
                 whiteMaterial,
@@ -104,7 +109,7 @@ export default class extends Controller {
         this.controls.dampingFactor = 0.1;
 
         // Variables pour gérer la rotation automatique
-        this.autoRotate = true;
+        this.autoRotate = false;
     }
 
     animate() {
@@ -145,54 +150,54 @@ export default class extends Controller {
         this.controls.update();
     }
 
-    showModalCanvas(event) {
-        // Création de la modale
-        const modalElement = document.createElement('div');
-        modalElement.classList.add('modal', 'fade');
-        modalElement.id = 'canvasModal';
-        modalElement.setAttribute('tabindex', '-1');
-        modalElement.setAttribute('aria-labelledby', 'canvasModalLabel');
-        modalElement.setAttribute('aria-hidden', 'true');
+    // showModalCanvas(event) {
+    //     // Création de la modale
+    //     const modalElement = document.createElement('div');
+    //     modalElement.classList.add('modal', 'fade');
+    //     modalElement.id = 'canvasModal';
+    //     modalElement.setAttribute('tabindex', '-1');
+    //     modalElement.setAttribute('aria-labelledby', 'canvasModalLabel');
+    //     modalElement.setAttribute('aria-hidden', 'true');
 
-        modalElement.innerHTML = `
-            <div class="modal-dialog modal-fullscreen">
-                <div class="modal-content">
-                    <div class="modal-header">
-                        <div>
-                            <button class="btn btn-secondary me-2" data-action="click->image#toggleRotation">Pause Rotation</button>
-                            <button class="btn btn-secondary me-2" data-action="click->image#resetCamera">Reset Camera</button>
-                        </div>
-                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                    </div>
-                    <div class="modal-body" style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
-                        <div id="modalCanvasContainer" style="width: 100%; height: 100%;"></div>
-                    </div>
-                </div>
-            </div>
-        `;
+    //     modalElement.innerHTML = `
+    //         <div class="modal-dialog modal-fullscreen">
+    //             <div class="modal-content">
+    //                 <div class="modal-header">
+    //                     <div>
+    //                         <button class="btn btn-secondary me-2" data-action="click->image#toggleRotation">Pause Rotation</button>
+    //                         <button class="btn btn-secondary me-2" data-action="click->image#resetCamera">Reset Camera</button>
+    //                     </div>
+    //                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+    //                 </div>
+    //                 <div class="modal-body" style="display: flex; justify-content: center; align-items: center; flex-direction: column;">
+    //                     <div id="modalCanvasContainer" style="width: 100%; height: 100%;"></div>
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     `;
 
-        document.body.appendChild(modalElement);
+    //     document.body.appendChild(modalElement);
 
-        // Afficher la modale
-        const modal = new Modal(modalElement);
-        modal.show();
+    //     // Afficher la modale
+    //     const modal = new Modal(modalElement);
+    //     modal.show();
 
-        modalElement.addEventListener('shown.bs.modal', () => {
-            const modalCanvasContainer = document.getElementById('modalCanvasContainer');
-            this.updateCanvasSize(modalCanvasContainer);
-            this.renderer.setSize(this.canvasWidth, this.canvasHeight);
-            modalCanvasContainer.appendChild(this.renderer.domElement);
-            this.camera.aspect = this.canvasWidth / this.canvasHeight;
-            this.camera.updateProjectionMatrix();
-        });
+    //     modalElement.addEventListener('shown.bs.modal', () => {
+    //         const modalCanvasContainer = document.getElementById('modalCanvasContainer');
+    //         this.updateCanvasSize(modalCanvasContainer);
+    //         this.renderer.setSize(this.canvasWidth, this.canvasHeight);
+    //         modalCanvasContainer.appendChild(this.renderer.domElement);
+    //         this.camera.aspect = this.canvasWidth / this.canvasHeight;
+    //         this.camera.updateProjectionMatrix();
+    //     });
 
-        modalElement.addEventListener('hidden.bs.modal', () => {
-            this.updateCanvasSize(this.canvasContainerTarget);
-            this.renderer.setSize(this.canvasWidth, this.canvasHeight);
-            this.canvasContainerTarget.appendChild(this.renderer.domElement);
-            this.camera.aspect = this.canvasWidth / this.canvasHeight;
-            this.camera.updateProjectionMatrix();
-            modalElement.remove();
-        });
-    }
+    //     modalElement.addEventListener('hidden.bs.modal', () => {
+    //         this.updateCanvasSize(this.canvasContainerTarget);
+    //         this.renderer.setSize(this.canvasWidth, this.canvasHeight);
+    //         this.canvasContainerTarget.appendChild(this.renderer.domElement);
+    //         this.camera.aspect = this.canvasWidth / this.canvasHeight;
+    //         this.camera.updateProjectionMatrix();
+    //         modalElement.remove();
+    //     });
+    // }
 }
